95c5aa9d0f8ba34edb53711c0d677901db5fdb66
Amr <frank1276smith@gmail.com>
2024-01-08 23:53:39 +0200

    changing the existing AboutUs component with a new one. I enhanced the style and it's responsive now

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,29 +1,34 @@
 import aboutImage from "../assets/images/about-bg.jpg";
+
 export default function AboutUs() {
   return (
-    <section
-      id="about-us"
-      className="h-screen flex justify-center items-center gap-8 px-7"
-    >
-      <div className="w-1/2">
-        <img src={aboutImage} alt="about" className="w-full rounded-3xl" />
+    <section className="lg:h-screen flex justify-between items-center max-lg:flex-col gap-10 w-full p-8  max-container">
+      <div className="flex flex-1 justify-center items-center">
+        <img
+          src={aboutImage}
+          alt="about-us"
+          className="object-contain rounded-3xl"
+        />
       </div>
-      <div className="w-1/2 p-8 rounded-3xl flex flex-col items-end text-right bg-[3c6e71]">
-        <h2 className=" text-5xl font-bold mb-3 text-emerald-950">
+
+      <div className="flex flex-1 flex-col items-end text-right ">
+        <h2 className=" text-5xl font-bold mb-3 text-emerald-950 lg:max-w-lg">
           معلومات عنا
         </h2>
-        <h3 className="text-4xl mb-2 text-slate-800">
+        <h3 className="text-4xl text-slate-800 lg:max-w-lg">
           لم يفت الأوان بعد لبدء العيش بشكل جيد
         </h3>
-        <p className="text-2xl my-2 leading-10">
+        <p className="mt-4 lg:max-w-lg text-xl leading-7 text-emerald-950">
           يهدف مشروعنا إلى إحداث ثورة في نظام الرعاية الصحية في مصر من خلال
           تقديم نظام تأمين صحي شامل مصحوب بتطبيق ويب وتطبيق جوال. تفتقر مصر
           حاليًا إلى نظام مركزي يتيح للمرضى الوصول إلى خدمات الرعاية الصحية مما
           يسبب الإزعاج والضغط للأفراد الذين يبحثون عن العلاج الطبي
         </p>
-        <button className="mt-3 px-2 py-2 bg-emerald-950 hover:bg-black text-white rounded-full w-36 text-xl font-bold">
-          التواصل معنا
-        </button>
+        <div>
+          <button className="mt-4 px-2 py-2 bg-emerald-950 hover:bg-black text-white rounded-full w-36 text-xl font-bold">
+            التواصل معنا
+          </button>
+        </div>
       </div>
     </section>
   );
