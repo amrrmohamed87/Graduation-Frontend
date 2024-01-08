974ba5c1775911bde62493da2d1e71c0ac70e7c0
Amr <frank1276smith@gmail.com>
2024-01-08 23:53:21 +0200

    changing the existing services component with a new one. I enhanced the style and it's responsive now

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,34 +1,36 @@
-import { servicesImages } from "../data/constants";
+import serviceImage from "../assets/images/service.png";
 
 export default function Services() {
   return (
-    <section id="about-us" className="h-screen flex  gap-24">
-      <div className="w-1/2 p-8 rounded-3xl flex flex-col items-end text-right bg-[3c6e71]">
-        <h2 className=" text-5xl font-bold mb-3 text-emerald-950">الخدمات</h2>
-        <h3 className="text-3xl mb-2 text-slate-800">
-          المعجزة ليست أننا نقوم بهذا العمل، ولكننا سعداء للقيام بذلك{" "}
+    <section className="lg:h-screen flex justify-between items-center max-lg:flex-col gap-10 w-full p-8  max-container">
+      <div className="flex flex-1 flex-col items-end text-right">
+        <h2 className=" text-5xl font-bold mb-3 text-emerald-950 lg:max-w-lg">
+          الخدمات
+        </h2>
+        <h3 className="text-3xl text-slate-800 lg:max-w-lg">
+          المعجزة ليست أننا نقوم بهذا العمل، ولكننا سعداء للقيام بذلك
         </h3>
-        <p className="text-2xl my-2 leading-10">
+        <p className="text-xl leading-7 mt-4 lg:max-w-lg">
           يقدم نظام التأمين الصحي المقترح خدمات رعاية صحية مريحة من خلال جدولة
           المواعيد مع الأطباء، ومعلومات الأدوية وتوافرها، والنصائح الصحية،
           والتصفية حسب أسماء الأطباء وتخصصاتهم
         </p>
-        <p className="text-2xl my-2 leading-10">
+        <p className="text-xl leading-7 mt-2 lg:max-w-lg">
           ويهدف البرنامج إلى تسهيل الوصول إلى الرعاية الصحية في مصر، وتعزيز
           التطبيب عن بعد، وتمكين الأفراد بالمعلومات، وتحسين صحة المجتمع بشكل عام
         </p>
-        <button className="mt-3 px-2 py-2 bg-emerald-950 hover:bg-black text-white rounded-full w-36 text-xl font-bold">
-          الخدمات
-        </button>
-      </div>
-      <div className="w-1/2">
-        <div className="flex flex-wrap">
-          {servicesImages.map((image) => (
-            <div key={image.image} className="p-2 ">
-              <img src={image.image} className="rounded-3xl" />
-            </div>
-          ))}
+        <div>
+          <button className="mt-3 px-2 py-2 bg-emerald-950 hover:bg-black text-white rounded-full w-36 text-xl font-bold">
+            الخدمات
+          </button>
         </div>
+      </div>
+      <div className="flex flex-1 justify-center items-center">
+        <img
+          src={serviceImage}
+          alt="service"
+          className="object-contain rounded-3xl"
+        />
       </div>
     </section>
   );
