15d2adb9092aa719d69912d4ebe8d1fbb3dea5da
Amr <frank1276smith@gmail.com>
2024-01-11 23:47:36 +0200

    I was trying to build a responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 import { navLinks } from "../data/constants";
 import logoImage1 from "../assets/images/MHI.png";
 import logoImage2 from "../assets/images/MHI-Emerald.svg";
+//import hamburger from "../assets/icons/hamburger.svg";
+
 export default function Navbar({ isScrolled }) {
   return (
     <header className="w-full z-10 bg-[#F7F5F2] absolute">
       <nav
-        className={`flex flex-1 justify-center items-center w-full fixed transition-all duration-300 ${
+        className={`flex justify-center items-centeR max-container w-full fixed transition-all duration-300 ${
           isScrolled ? "bg-[#F7F5F2] shadow-md" : "bg-transparent shadow-none"
         }`}
       >
@@ -26,7 +30,7 @@ export default function Navbar({ isScrolled }) {
                   : "text-xl text-slate-300 hover:text-slate-100"
               }
             >
-              <a href={link.href}>{link.label}</a>
+              <Link to={link.to}>{link.label}</Link>
             </li>
           ))}
         </ul>
@@ -39,6 +43,10 @@ export default function Navbar({ isScrolled }) {
         >
           تسجيل الدخول
         </button>
+
+        {/*  <div className="hidden max-lg:block fixed">
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div> */}
       </nav>
     </header>
   );
