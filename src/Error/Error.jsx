d2f65dccdec76a1e67dc2ca4930b99ce3de08e9c
Amr <frank1276smith@gmail.com>
2024-01-12 23:23:42 +0200

    Adding a button that navigate back to the previous page

--- src/Error/Error.jsx
+++ src/Error/Error.jsx
@@ -1,15 +1,19 @@
-import Navbar from "../components/Navbar";
-
+import { Link } from "react-router-dom";
 export default function Error() {
   return (
     <>
-      <main className="m-16">
+      <main className="m-16 flex flex-col justify-center items-center">
         <h1 className="font-bold text-3xl text-emerald-950 text-center">
-          An error Occurred!
+          حدث خطأ
         </h1>
-        <p className="text-2xl leading-7 text-center">
-          Could not find this page
+        <p className="text-2xl text-slate-600 text-center">
+          لا يمكن العثور على هذه الصفحة
         </p>
+        <button className="mt-4 bg-emerald-950 p-3 rounded-2xl text-white">
+          <Link to=".." relative="path">
+            العودة إلى الصفحة الرئيسية
+          </Link>
+        </button>
       </main>
     </>
   );
