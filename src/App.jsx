9da9ee26ea76242b7cdd938e5158d79f7b23b3fb
Amr <frank1276smith@gmail.com>
2024-02-25 02:49:09 +0200

    some changes - amr

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import RootLayout from "./root/RootLayout";
 import Home from "./pages/Home";
 import Error from "./Error/Error";
 import ContactUs from "./pages/ContactUs.jsx";
-import Login from "./pages/Login.jsx";
-import Signin from "./pages/Signin.jsx";
-import NewLogin from "./pages/NewLogin.jsx";
-import { action as authAction } from "./pages/NewLogin.jsx";
+
 import { Service } from "./pages/Service.jsx";
 import { MedSearch } from "./pages/MedSearch.jsx";
 import { DocSearch } from "./pages/DocSearch.jsx";
@@ -25,38 +22,44 @@ import DuringPregnancy from "./health/woman/DuringPregnancy.jsx";
 import Birth from "./health/woman/Birth.jsx";
 import Elderly from "./health/senior/Elderly.jsx";
 import Alzheimers from "./health/senior/Alzheimers.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    errorElement: <Error />,
-    id: "root",
-    loader: tokenLoader,
-    children: [
-      { path: "", element: <Home /> },
-      { path: "contact-us", element: <ContactUs /> },
-      { path: "service", element: <Service /> },
-      { path: "searchmed", element: <MedSearch/> },
-      { path: "docsearch", element: <DocSearch/> },
-      { path: "health-awareness", element: <HealthAwareness /> },
-      { path: "ca", element: <Nutrition /> },
-      { path: "logout", action: logoutAction },
-    ],
-  },
-  { path: "login", element: <NewLogin />, action: authAction },
-  { path: "child-safety", element: <ChildSafety /> },
-  { path: "vaccinations", element: <Vaccinations /> },
-  { path: "school-health", element: <SchoolHealth /> },
-  { path: "baby-nutrition", element: <BabyNutrition /> },
-  { path: "before-pregnancy", element: <BeforePregnancy /> },
-  { path: "during-pregnancy", element: <DuringPregnancy /> },
-  { path: "after-pregnancy", element: <AfterPregnancy /> },
-  { path: "birth", element: <Birth /> },
-  { path: "care-of-the-elderly", element: <Elderly /> },
-  { path: "alzheimers", element: <Alzheimers /> },
-]);
+import DoctorProfile from "./pages/DoctorProfile.jsx";
+import Admin from "./pages/Admin.jsx";
+import Hospital from "./pages/Hospital.jsx";
+import Login from "./pages/Login.jsx";
 
 export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      errorElement: <Error />,
+      id: "root",
+      loader: tokenLoader,
+      children: [
+        { path: "", element: <Home /> },
+        { path: "health-awareness", element: <HealthAwareness /> },
+        { path: "ca", element: <Nutrition /> },
+        { path: "service", element: <Service /> },
+        { path: "searchmed", element: <MedSearch /> },
+        { path: "docsearch", element: <DocSearch /> },
+        { path: "contact-us", element: <ContactUs /> },
+        { path: "logout", action: logoutAction },
+      ],
+    },
+    { path: "/login", element: <Login /> },
+    { path: "/doctor-profile", element: <DoctorProfile /> },
+    { path: "/admin", element: <Admin /> },
+    { path: "/hospital", element: <Hospital /> },
+    { path: "child-safety", element: <ChildSafety /> },
+    { path: "vaccinations", element: <Vaccinations /> },
+    { path: "school-health", element: <SchoolHealth /> },
+    { path: "baby-nutrition", element: <BabyNutrition /> },
+    { path: "before-pregnancy", element: <BeforePregnancy /> },
+    { path: "during-pregnancy", element: <DuringPregnancy /> },
+    { path: "after-pregnancy", element: <AfterPregnancy /> },
+    { path: "birth", element: <Birth /> },
+    { path: "care-of-the-elderly", element: <Elderly /> },
+    { path: "alzheimers", element: <Alzheimers /> },
+  ]);
   return <RouterProvider router={router} />;
 }
