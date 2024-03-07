7b5d410497b7afc85cd4378f3b4e976b01d78b87
Amr <frank1276smith@gmail.com>
2024-03-07 11:36:37 +0200

    .

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 import "../css/Booking.css";
-/* import BgPhoto from "../assets/images/1.jpg"
-import sectionsPhoto1 from "../assets/images/pexels-photo-5155774.webp"
-import sectionsPhoto2 from "../assets/images/realheart.jpg"
-import sectionsPhoto3 from "../assets/images/ear.webp"
-import sectionsPhoto4 from "../assets/images/kids.jpg"
-import sectionsPhoto5 from "../assets/images/skin.jpg"
-import sectionsPhoto6 from "../assets/images/foto_upload_1525844034.jpg"
-import sectionsPhoto7 from "../assets/images/2022-637854515927417334-741.jpg"
-import sectionsPhoto8 from "../assets/images/فحص-العين-768x456.jpg"
-import sectionsPhoto9 from "../assets/images/endocr.png"
-import sectionsPhoto10 from "../assets/images/radyasyononkolojisiradyoterapijpg_0a2d.webp"
-import sectionsPhoto11 from "../assets/images/دكتور-نفسي-بالمنزل-ميديكال-كير.jpg"
-import sectionsPhoto12 from "../assets/images/6303b6d32a466abd05dcf325_Neurology.jpg"
-import sectionsPhoto13 from "../assets/images/symptoms-similarity-of-digestive-system-diseases.jpg"
-import sectionsPhoto14 from "../assets/images/الأمراض-المتناقلة-جنسيًا-ft-img.webp"
-import sectionsPhoto15 from "../assets/images/1654546_0.jpg"
-import sectionsPhoto16 from "../assets/images/download.jfif"
-import Footer from './../components/Footer';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import BgPhoto from "../assets/images/1.jpg";
+import sectionsPhoto1 from "../assets/images/pexels-photo-5155774.webp";
+import sectionsPhoto2 from "../assets/images/realheart.jpg";
+import sectionsPhoto3 from "../assets/images/ear.webp";
+import sectionsPhoto4 from "../assets/images/kids.jpg";
+import sectionsPhoto5 from "../assets/images/skin.jpg";
+import sectionsPhoto6 from "../assets/images/foto_upload_1525844034.jpg";
+import sectionsPhoto7 from "../assets/images/2022-637854515927417334-741.jpg";
+import sectionsPhoto8 from "../assets/images/فحص-العين-768x456.jpg";
+import sectionsPhoto9 from "../assets/images/endocr.png";
+import sectionsPhoto10 from "../assets/images/radyasyononkolojisiradyoterapijpg_0a2d.webp";
+import sectionsPhoto11 from "../assets/images/دكتور-نفسي-بالمنزل-ميديكال-كير.jpg";
+import sectionsPhoto12 from "../assets/images/6303b6d32a466abd05dcf325_Neurology.jpg";
+import sectionsPhoto13 from "../assets/images/symptoms-similarity-of-digestive-system-diseases.jpg";
+import sectionsPhoto14 from "../assets/images/الأمراض-المتناقلة-جنسيًا-ft-img.webp";
+import sectionsPhoto15 from "../assets/images/1654546_0.jpg";
+import sectionsPhoto16 from "../assets/images/download.jfif";
+import Footer from "./../components/Footer";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 export function Booking() {
   const cardData = [
     {
@@ -114,10 +114,10 @@ export function Booking() {
     <>
       <header className="relative h-screen w-full">
         <div className=" w-full h-screen">
-          {/* <img
+          <img
             src={BgPhoto}
             className="object-cover object-center h-screen w-full"
-          />*/}
+          />
         </div>
         <div className="absolute inset-0 bg-black opacity-80"></div>
         <div className="absolute inset-2 left-4 md:inset-0 flex flex-1 flex-col justify-center text-right mr-2 md:mr-0 md:items-center">
@@ -143,11 +143,11 @@ export function Booking() {
                 key={i}
                 className="col-md-4  rounded-5 mb-5 position-relative layer cardSize "
               >
-                {/* <img
+                <img
                   src={element.imgSrc}
                   alt="photo"
                   className="rounded-5 w-100 h-100"
-                /> */}
+                />
 
                 <div className="position-absolute start-0 bg-black layerUp rounded-5 h-100 w-100 opacity-0 top-0 ">
                   <div>
@@ -171,11 +171,22 @@ export function Booking() {
               <p>اغلاق</p>
             </div>
             <div className="col-md-6 py-3">
-              <label htmlFor="" className='me-2  text-end d-block col-form-label mt-2'>احجز التاريخ</label>
-              <DatePicker selected={selectedDate} onChange={date => setSelectedDate(date)}className='form-control d-block text-end'></DatePicker>
-              <label htmlFor="" className='d-block col-form-label text-end'>احجز الوقت</label>
-              <input type="time" className='form-control w-25'/>
-              <button className='btn btn-success mt-3'>احجز الان</button>
+              <label
+                htmlFor=""
+                className="me-2  text-end d-block col-form-label mt-2"
+              >
+                احجز التاريخ
+              </label>
+              <DatePicker
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
+                className="form-control d-block text-end"
+              ></DatePicker>
+              <label htmlFor="" className="d-block col-form-label text-end">
+                احجز الوقت
+              </label>
+              <input type="time" className="form-control w-25" />
+              <button className="btn btn-success mt-3">احجز الان</button>
             </div>
             <div className="col-md-6 text-end py-3">
               <h1 className="selectTag fs-2"></h1>
--- src/pages/DocSearch.jsx
+++ src/pages/DocSearch.jsx
@@ -1,67 +1,87 @@
-import React, { useEffect, useState } from 'react'
-import "../css/Search.css"
-import Footer from '../components/Footer'
-import SearchPhoto from "../assets/images/search.jpg"
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import "../css/Search.css";
+import Footer from "../components/Footer";
+import SearchPhoto from "../assets/images/search.jpg";
+import axios from "axios";
 export function DocSearch() {
-    let [DocData, setDocData] = useState([])
+  let [DocData, setDocData] = useState([]);
 
-    useEffect(() => {
-        getDoctors()
+  useEffect(() => {
+    getDoctors();
+  }, []);
+  async function getDoctors() {
+    let { data } = await axios.get(
+      "https://mhiproject.onrender.com/patient/getDoctors"
+    );
+    console.log(data.userD);
+    setDocData(data.userD);
+  }
 
-    }, [])
-    async function getDoctors() {
-        let { data } = await axios.get('https://mhiproject.onrender.com/patient/getDoctors')
-        setDocData(data.userD)
-    }
-
-
-
-
-    return (
-        <>
-            <header className="relative h-screen w-full ">
-                <div className=" w-full h-screen">
-                    <img src={SearchPhoto} className="object-cover object-center h-screen w-full" />
+  return (
+    <>
+      <header className="relative h-screen w-full ">
+        <div className=" w-full h-screen">
+          <img
+            src={SearchPhoto}
+            className="object-cover object-center h-screen w-full"
+          />
+        </div>
+        <div className="absolute inset-0 bg-black opacity-80"></div>
+        <div className="absolute inset-2 left-4 md:inset-0 flex flex-1 flex-col justify-center text-right mr-2 md:mr-0 md:items-center">
+          <h2 className="text-white text-[80px]"> البحث </h2>
+          <p className="text-slate-300 text-[40px] mt-2 md:mt-0 opacity-50">
+            {" "}
+            توفير معرفة أعمق تساعد الأشخاص في جميع المعلومات التي يحتاجونها{" "}
+          </p>
+        </div>
+      </header>
+      <section className="py-8 flex justify-content-center align-items-center bg-white">
+        <div className="container-xxl">
+          <div className="flex flex-column justify-content-evenly justify-content-center align-items-center text-center">
+            <div className=" ">
+              <h2 className="text-black text-[80px]"> الاطباء </h2>
+              <div class="input-group mb-3">
+                <button
+                  class="btn btn-outline-secondary"
+                  type="button"
+                  id="button-addon1"
+                >
+                  ابحث
+                </button>
+                <input
+                  type="text"
+                  class="form-control text-right"
+                  placeholder="...بحث"
+                  aria-label="Example text with button addon"
+                  aria-describedby="button-addon1"
+                />
+              </div>
+            </div>
+            {/* this div for result of the search  */}
+            <div className="w-50 mt-4 text-right p-4 border-3 border-black rounded-5 ">
+              <h2 className="text-black textStyleForH2"> احمد بخة </h2>
+              <p className="mt-2 md:mt-0 textStyleForP"> دكتور عيون </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="py-8 bg-white">
+        <div className="container">
+          <div className="row py-3 ">
+            {DocData.map((element, i) => (
+              <div key={i} className="col-md-4 rounded-5 border-4 mt-3">
+                <div className="py-3 ps-2 text-end">
+                  <h1 className="fs-3">{element.name}</h1>
+                  <span className="fs-5 ">id</span>
+                  <h6 className="fs-6"> {element._id} </h6>
+                  <h3 className="fs-5">{element.specialize} </h3>
                 </div>
-                <div className="absolute inset-0 bg-black opacity-80"></div>
-                <div className="absolute inset-2 left-4 md:inset-0 flex flex-1 flex-col justify-center text-right mr-2 md:mr-0 md:items-center">
-                    <h2 className="text-white text-[80px]"> البحث </h2>
-                    <p className="text-slate-300 text-[40px] mt-2 md:mt-0 opacity-50"> توفير معرفة أعمق تساعد الأشخاص في جميع المعلومات التي يحتاجونها </p>
-                </div>
-            </header>
-            <section className='py-8 flex justify-content-center align-items-center bg-white '>
-                <div className='container-xxl'>
-                    <div className='flex flex-column justify-content-evenly justify-content-center align-items-center text-center'>
-                        <div className=' '>
-                            <h2 className="text-black text-[80px] mb-3"> الاطباء </h2>
-                            <form class="input-group mb-3">
-                                <button class="btn btn-outline-secondary" type="button" id="button-addon1">ابحث</button>
-                                <input type="text" class="form-control text-right " placeholder="...بحث" aria-label="Example text with button addon" aria-describedby="button-addon1" />
-                            </form>
-                        </div>
-                        {/* this div for result of the search  */}
-                        <div className='w-50 mt-4 text-right p-4 border-3 border-black rounded-5 '>
-                            <h2 className="text-black textStyleForH2"> احمد بخة  </h2>
-                            <p className="mt-2 md:mt-0 textStyleForP"> دكتور عيون  </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className='py-8 bg-white'>
-                <div className="container">
-                    <div className="row py-3 ">
-                        {DocData.map((element, i) => <div key={i} className="col-md-4 rounded-5 border-4 mt-3">
-                            <div className='py-3 ps-2 text-end'>
-                                <h1 className='fs-3 mb-2'>{element.name}</h1>
-                                <h6 className='fs-6 mb-2'> {element._id} </h6>
-                                <h3 className='fs-5 mb-2'>{element.specialize} </h3>
-                            </div>
-                        </div>)}
-                    </div>
-                </div>
-            </section>
-            <Footer />
-        </>
-    )
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
