67fa9bd59c6ea418151d5c49243dc6bad14dc476
rameeezz <ramezhossam92@gmail.com>
2024-03-22 21:16:52 +0200

    ...........

--- src/pages/MedSearch.jsx
+++ src/pages/MedSearch.jsx
@@ -16,26 +16,38 @@ export function MedSearch() {
           <p className="text-slate-300 text-[40px] mt-2 md:mt-0 opacity-50"> توفير معرفة أعمق تساعد الأشخاص في جميع المعلومات التي يحتاجونها </p>
         </div>
       </header>
+      {/* header alsf7a */}
       <section>
-          {/* header alsf7a */}
         <div className='container'>
           <div className='w-100 flex-wrap d-flex justify-content-center'>
-          <div className='w-100 d-flex justify-content-center'>
-          <h1 className="text-center text-[#056558] mt-12 text-[22px] md:text-[30px] w-50">البحث عن الأدوية يمثل </h1>
-          </div>
-          <h1 className="text-center mt-4 text-[22px] md:text-[30px] w-50">"رحلة  من التفاؤل و الثقة ،حيث تمكن فى كل قرار صحي حكمة واهتمام بالصحة الشخصية"</h1>
-          </div>
-            <div className='d-flex w-100 gap-5 mt-5  py-3 justify-content-around'>
-            <div className=' DivSoraSearchMed rounded-4 shadow-lg'> 
-              <img src={SectionPhoto} alt="sora" className='rounded-4'/>
-              </div>
-              <div className='w-50 shadow-lg p-4 rounded-4'>
-                <h1 className='text-end fs-1'>هدفنا هو التوعية <i className="fa-solid fs-3 text-success fa-magnifying-glass"></i></h1>
-                <p className='fs-3 mt-5 text-end '>قم بتنزيل تطبيقنا الآن، حيث تجتمع الراحة مع التكنولوجيا المتطورة! افتح عالمًا من الإمكانيات من خلال تطبيقنا سهل الاستخدام الذي يلبي جميع احتياجاتك. سواء كنت أثناء التنقل أو مسترخي في المنزل، فقد تم تصميم تطبيقنا لجعل تجربتك سلسة وممتعة
-                </p>
-              </div>
+            <div className='w-100 d-flex justify-content-center'>
+              <h1 className="text-center text-[#056558] mt-12 text-[22px] md:text-[30px] w-50">البحث عن الأدوية يمثل </h1>
             </div>
-          
+            <h1 className="text-center mt-4 text-[22px] md:text-[30px] w-50">"رحلة  من التفاؤل و الثقة ،حيث تمكن فى كل قرار صحي حكمة واهتمام بالصحة الشخصية"</h1>
+          </div>
+          <div className='d-flex w-100 gap-5 mt-5  py-3 justify-content-around'>
+            <div className=' DivSoraSearchMed rounded-4 shadow-lg'>
+              <img src={SectionPhoto} alt="sora" className='rounded-4' />
+            </div>
+            <div className='w-50 shadow-lg p-4 rounded-4'>
+              <h1 className='text-end fs-1'>هدفنا هو التوعية <i className="fa-solid fs-3 text-success fa-magnifying-glass"></i></h1>
+              <p className='fs-3 mt-5 text-end '>قم بتنزيل تطبيقنا الآن، حيث تجتمع الراحة مع التكنولوجيا المتطورة! افتح عالمًا من الإمكانيات من خلال تطبيقنا سهل الاستخدام الذي يلبي جميع احتياجاتك. سواء كنت أثناء التنقل أو مسترخي في المنزل، فقد تم تصميم تطبيقنا لجعل تجربتك سلسة وممتعة
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* search aladwya */}
+      <section className='py-5'>
+        <div className='container'>
+          <form>
+            <div className='d-flex w-100 justify-content-center '>
+              <button type='button' className='btn btn-info rounded-5'>
+                <i className="fa-solid fs-3 text-info fa-magnifying-glass"></i>
+              </button>
+              <input type="text" className='w-50 form-control text-end  rounded-5' placeholder='.....ابحث عن الادوية'/>
+            </div>    
+          </form>
         </div>
       </section>
       <Footer />
