ea4eaa0d3363721ad3ed725c133d5bcd99997317
Amr <frank1276smith@gmail.com>
2024-03-23 17:23:56 +0200

    enhancing input style - amr

--- src/components/AdminInput.jsx
+++ src/components/AdminInput.jsx
@@ -1,14 +1,14 @@
 function Input({ id, label, ...props }) {
   return (
     <div className="flex flex-col justify-start items-end mb-4">
-      <label htmlFor={id} className="text-white mb-2">
+      <label htmlFor={id} className="text-gray-800 mb-2">
         {label}
       </label>
       <input
         id={id}
         required
         {...props}
-        className="h-[30px] border-2 border-emerald-700 focus:border-gray-950 rounded-lg pl-2
+        className="h-[30px] w-full border-2 border-gray-300 focus:border-gray-950 rounded-lg pl-2
         md:h-[40px]"
       />
     </div>
