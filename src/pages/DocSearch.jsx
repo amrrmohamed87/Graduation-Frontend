90925602d15f8a05075b693bc06882d34a9ff9e2
rameeezz <ramezhossam92@gmail.com>
2024-03-23 03:42:43 +0200

    search and booking

--- src/pages/DocSearch.jsx
+++ src/pages/DocSearch.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import "../css/Search.css";
 import Footer from "../components/Footer";
 import SearchPhoto from "../assets/images/search.jpg";
+import firstSectionPhoto from "../assets/images/Concept Médical Médecin Et Patient Dans La Salle Intérieure De L'hôpital _ Vecteur Premium.jfif"
+import secondSectionPhoto from "../assets/images/Download Cardiologists Doctor Pointing at Heart Diagram for free.jfif"
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 // import { data } from "autoprefixer";
@@ -18,6 +20,12 @@ export function DocSearch() {
     })
     const [sureBookSection, setSureBookSection] = useState("d-none");
     const [ClassNamee, setClassName] = useState("d-none position-fixed z-1 secBook rounded-5 shadow-lg bg-white");
+    // -----------------------------new Code ------------------------
+    const [showDivSearch, setShowDivSearch] = useState('d-none')
+    const [isSearchActive, setIsSearchActive] = useState(false);
+    const [showDiv, setShowDiv] = useState(false);
+    const [showDiv2, setShowDiv2] = useState(false);
+    // -----------------------------new code ------------------------
     let [DocData, setDocData] = useState([])
     let [showResult, setShowResult] = useState([])
     let [docDetail, setDocDetail] = useState({
@@ -30,7 +38,6 @@ export function DocSearch() {
     let [trueBook, setTrueBook] = useState("d-none")
     const [error, setError] = useState('');
     const [classOfError, setClassOfError] = useState("text-center fs-5 text-danger")
-    // const [error2, setError2] = useState('');
     let [BookTime, setBookTime] = useState({
         day: "",
         time: "",
@@ -54,9 +61,9 @@ export function DocSearch() {
             let { data } = await axios.get('https://mhiproject.onrender.com/patient/getDoctors')
             setDocData(data.userD)
         } catch (errorGetDoc) {
-            if (errorGetDoc.data.status == null) {
+            if (errorGetDoc.data.status === 404) {
                 setErrorGetDoc("text-center fs-1")
-            }else{
+            } else {
                 setErrorGetDoc("text-center fs-1")
             }
         }
@@ -69,17 +76,24 @@ export function DocSearch() {
         let MyUser = { ...searchUser }
         MyUser[e.target.name] = e.target.value
         setSearchUser(MyUser)
+        console.log(MyUser);
     }
     function HideSureBoook() {
         setSureBookSection("d-none")
     }
     async function submitSearch(e) {
         e.preventDefault()
-        let { data } = await axios.post("https://mhiproject.onrender.com/patient/search", searchUser)
-        setShowResult(data.search)
-        if (data.search == null) {
-            setErrorForSearch("text-center fs-1 mt-5")
+        try {
+            let { data } = await axios.post("https://mhiproject.onrender.com/patient/search", searchUser)
+            setShowResult(data.search)
+        } catch (errorForSearch) {
+            if (errorForSearch.data.search === 404) {
+                setErrorForSearch("text-center fs-1 mt-5")
+            } else {
+                setErrorForSearch("text-center fs-1 mt-5")
+            }
         }
+
 
     }
     function ShowBookSection(IDdoc, DocName) {
@@ -122,6 +136,12 @@ export function DocSearch() {
                 setErrorButton("btn btn-success me-5")
                 setClassOfError("text-center fs-5 text-danger")
             }
+            if (error.data && error.data.status === 400) {
+                setError('يرجى ادخال بيانات صحيحة ');
+
+            } else {
+                setError('يرجى ادخال بيانات صحيحة ');
+            }
             if (error.data && error.data.status === 404) {
                 setError('يرجى اختيار موعد حجز أخر');
                 setErrorButton("btn btn-success me-5")
@@ -132,12 +152,7 @@ export function DocSearch() {
                 setClassOfError("text-center fs-5 text-danger")
             }
 
-            if (error.data && error.data.status === 400) {
-                setError('يرجى ادخال بيانات صحيحة ');
 
-            } else {
-                setError('يرجى ادخال بيانات صحيحة ');
-            }
         }
     }
     function BackStep() {
@@ -146,68 +161,35 @@ export function DocSearch() {
         setErrorButton("d-none")
         setClassOfError("d-none")
     }
-
+    // ---------------------new code-----------------
+    function showSectionSearch() {
+        setShowDivSearch('container position-relative mb-5 mt-5')
+    }
+    function CloseSectionSearch() {
+        setShowDivSearch('d-none')
+    }
+    const toggleDivPosition = () => {
+        if (!isSearchActive) {
+            setShowDivSearch('searchSection active');
+            setIsSearchActive(true);
+        }
+    };
+    const toggleDivPosition2 = () => {
+        setShowDiv(!showDiv);
+    };
+    const toggleDivPosition3 = () => {
+        setShowDiv2(!showDiv2);
+    };
+    // -----------------------------------------------
     return (
         <>
             <header className="relative h-screen w-full ">
                 <div className=" w-full h-screen">
                     <img src={SearchPhoto} className="object-cover object-center h-screen w-full" />
                 </div>
-                <div className="absolute inset-0 bg-black opacity-80"></div>
                 <div className="absolute inset-1 left-4 md:inset-0 flex flex-1 flex-col justify-center text-right mr-2 md:mr-0 md:items-center">
-                    <h2 className="text-white text-[80px]"> البحث </h2>
-                    <p className="text-slate-300 text-[40px] mt-2 md:mt-0 opacity-50"> توفير معرفة أعمق تساعد الأشخاص في جميع المعلومات التي يحتاجونها </p>
                 </div>
             </header>
-            {/* section alsearch */}
-            <section className='py-8 UnderLineBetweenSection position-relative'>
-                <div className='container'>
-                    <div className='flex flex-row flex-wrap justify-content-evenly justify-content-center align-items-center text-center'>
-                        <div className=' w-100 justify-content-center d-flex'>
-                            <div className="w-50 ">
-                                <h2 className="text-black text-[80px] mb-3"> الأطباء </h2>
-                                <form className="input-group mb-3">
-                                    <button className="btn btn-outline-secondary" type="button" onClick={submitSearch}>ابحث</button>
-                                    <input type="text" className="form-control text-right " onChange={searchForDoctor} placeholder=".....بحث باسم الدكتور" name='name' />
-                                    <input type="text" className="form-control text-right " onChange={searchForDoctor} placeholder=".....بحث بالتخصص" name='specialize' />
-                                </form>
-                            </div>
-                        </div>
-                        <h1 className={errorForSearch}>No input please write name or specialize</h1>
-                        {/* this div for result of the search  */}
-                        {showResult == null ? <div className="alert alert-danger text-center text-lg-center">not found</div> :<div className="d-flex justify-content-center flex-wrap gap-3 overflow-scroll w-100 searchSection position-relative"> {showResult.map((element, i) => <div key={i} className=' w-25 mt-4 text-right p-4 border-3 border-success rounded-4'>
-                            <h2 className="text-center fs-3 text-muted">د/{element.name}  </h2>
-                            <p className="mt-2 md:mt-0 fs-4 text-muted"><span className="fs-3 text-black"></span>{element.specialize} : <i class="fa-solid text-success fs-5 fa-stethoscope"></i></p>
-                            <p className="mt-2 md:mt-0 fs-4 text-muted"> {element.hospitalID?.name} : <i className=" fa-solid text-success fs-5 fa-truck-medical"></i> </p>
-                            <div className="d-flex justify-content-center"> <button onClick={() => ShowBookSection(element._id, element.name)} type="button" className="btn w-100 bg-success text-white border-3">احجز الان</button></div>
-                        </div>)}
-                        </div>
-                        }
-
-                    </div>
-                </div>
-            </section>
-            {/* section get doctors */}
-            <section className='py-8'>
-                <div className="container">
-                    <div className="row py-3 gap-3 justify-content-center">
-                        <h1 className={errorGetDoc}>جارى التحميل</h1>
-                        {DocData.map((element, i) => <div key={i} className="col-md-3 rounded-4 border-4 mt-3">
-                            <div className='py-3 text-end'>
-                                <h1 className='text-center fs-3 mb-2 text-[#056558]'>د/ {element.name}</h1>
-                                <h3 className='fs-3 mb-2'>{element.specialize} : <i className="fa-solid text-success fs-5 fa-stethoscope"></i>  </h3>
-                                <div className="d-flex gap-2 flex-row justify-content-end">
-                                    <h6 className='fs-5'>  {element.hospitalID?.name} : </h6>
-                                    <i className=" fa-solid text-success fs-5 fa-truck-medical"></i>
-                                </div>
-                                <div className="d-flex mt-3 justify-content-center">
-                                    <button onClick={() => ShowBookSection(element._id, element.name)} type="button" className="btn bg-success  w-100 text-white border-3">احجز الان</button>
-                                </div>
-                            </div>
-                        </div>)}
-                    </div>
-                </div>
-            </section>
             {/* da section al7gz */}
             <section>
                 <div className={ClassNamee}>
@@ -440,13 +422,6 @@ export function DocSearch() {
 
                             </div>
                         </div>
-
-
-                        {/* <label htmlFor="doctorID" className="me-3 d-block col-form-label fs-2 text-end">
-                            : اسم المستشفى
-                        </label>
-                        <h1 className="text-xxl-center fs-3 ">   </h1> */}
-                        {/* <p className="text-center text-danger fs-5">{error2}</p> */}
                         <div className="d-flex mt-4 justify-content-center gap-5">
                             <button onClick={() => FirstSubmitBook(docDetail.id)} type="button" className="btn btn-success text-black  ">احجز الان</button>
                             <button onClick={() => closeBookSection()} type="button" className="btn btn-danger text-black ">اغلاق</button>
@@ -490,6 +465,87 @@ export function DocSearch() {
                 <button onClick={() => closeTrueBook()} type="button" className="btn btn-danger text-black ms-3 ">اغلاق</button>
             </div>
             {/* ------------------------------------------------- */}
+            <section className="container mt-5">
+                <div className="row  justify-content-center">
+                    <div className="col-md-5 layerfixed rounded-4 position-relative">
+                        <img src={firstSectionPhoto} alt="" className="w-75 h-100 rounded-4 shadow position-absolute layerGoUp z-2" />
+                    </div>
+                    <div className="col-md-5 position-relative rounded-4 shadow p-4">
+                        <div className="d-flex flex-wrap w-100 justify-content-end">
+                            <h1 className="text-end fs-1 mb-4"> أختر الدكتور المناسب</h1>
+                            <i className="fa-solid fs-3 text-success opacity-75 fa-magnifying-glass mt-2 ms-2"></i>
+                        </div>
+                        <p className="text-end fs-4 text-muted mb-5"> يوجد جميع التخصصات نتيح لكم أن تبحثوا بالأسم و التخصص واجبنا أن نسهل عليكم جميع الخدمات</p>
+                        <button onClick={showSectionSearch} type="button" className="position-absolute btn text-white bg-success mb-3 start-25 bottom-0"> ابحث الان
+                        </button>
+                    </div>
+
+                </div>
+            </section>
+            {/* section search and response of search  */}
+            <section className={showDivSearch}>
+
+                <button onClick={CloseSectionSearch} className="btn btn-close position-absolute fs-5 end-0"></button>
+                <form className="mt-5">
+                    <div className='d-flex w-100 justify-content-center '>
+                        <button onClick={(e) => {
+                            submitSearch(e);
+                            toggleDivPosition()
+                        }} type='button' className='btn btn-info rounded-5'>
+                            <i className="fa-solid fs-3 text-info fa-magnifying-glass"></i>
+                        </button>
+                        <input type="text" className='w-25 form-control text-end  rounded-5' onChange={searchForDoctor} placeholder=".....بحث باسم الدكتور" name='name' />
+                        <input type="text" className='w-25 form-control text-end  rounded-5' onChange={searchForDoctor} placeholder=".....بحث بالتخصص" name='specialize' />
+                    </div>
+                </form>
+                {/* result of search */}
+                <h1 className={errorForSearch}>No input please write name or specialize</h1>
+                {showResult == null ? <div className="alert alert-danger text-center text-lg-center">not found</div> : <div className={`d-flex justify-content-center mt-3 flex-wrap gap-3 overflow-scroll w-100 searchSection position-relative ${isSearchActive ? 'active' : ''}`}
+                > {showResult.map((element, i) => <div key={i} className=' w-25 mt-4 text-right p-4 border-3 border-success rounded-4'>
+                    <h2 className="text-center fs-3 text-muted">د/{element.name}  </h2>
+                    <p className="mt-2 md:mt-0 fs-4 text-muted"><span className="fs-3 text-black"></span>{element.specialize} : <i class="fa-solid text-success fs-5 fa-stethoscope"></i></p>
+                    <p className="mt-2 md:mt-0 fs-4 text-muted"> {element.hospitalID?.name} : <i className=" fa-solid text-success fs-5 fa-truck-medical"></i> </p>
+                    <div className="d-flex justify-content-center"> <button onClick={() => ShowBookSection(element._id, element.name)} type="button" className="btn mt-3 w-100 bg-success text-white border-3">احجز الان</button></div>
+                </div>)}
+                </div>
+                }
+            </section>
+            {/* section Get doctors */}
+            <section className="mt-5 z-2 HightForSlidedown position-relative overflow-hidden">
+                <div className="container  mt-5 d-flex flex-wrap justify-content-center">
+                    <div className="w-75 shadow rounded-4 p-4">
+                        <h1 className="fs-4 text-center">يمكنك بسهوله عرض جميع الأطباء بالضغط على هذا الزر</h1>
+                        <div className="w-100 d-flex gap-5 justify-content-center mt-5">
+                            <button type="button" onClick={()=>{
+                                toggleDivPosition2()
+                                toggleDivPosition3()
+                            } } className="btn text-white bg-success">أعرض الأطباء</button>
+                            <button type="button" onClick={() => {
+                                toggleDivPosition2()
+                                toggleDivPosition3()
+                                
+                            }} className="btn text-white bg-success">أخفاء الأطباء</button>
+                        </div>
+                    </div>
+                    <img src={secondSectionPhoto} className={`position-absolute StyleSoraGetSection rounded-4 ${showDiv ? 'active' : ''}`} alt="sora" />
+                    <div className={`w-75 shadow rounded-4 z-3 bottom-0  d-flex flex-wrap justify-content-around gap-4 position-absolute overflow-scroll styleOfBoxGetUsers ${showDiv ? 'active' : ''}`}>
+                        <h1 className={errorGetDoc}>جارى التحميل</h1>
+                        {DocData.map((element, i) => <div key={i} className="widthForDivInGetUser rounded-4 border-4 mt-3">
+                            <div className='py-3 text-end'>
+                                <h1 className='text-center fs-3 mb-2 text-[#056558]'>د/ {element.name}</h1>
+                                <h3 className='fs-3 mb-2'>{element.specialize} : <i className="fa-solid text-success fs-5 fa-stethoscope"></i>  </h3>
+                                <div className="d-flex gap-2 flex-row justify-content-end">
+                                    <h6 className='fs-5'>  {element.hospitalID?.name} : </h6>
+                                    <i className=" fa-solid text-success fs-5 fa-truck-medical"></i>
+                                </div>
+                                <div className="d-flex mt-3 justify-content-center">
+                                    <button onClick={() => ShowBookSection(element._id, element.name)} type="button" className="btn bg-success  w-100 text-white border-3">احجز الان</button>
+                                </div>
+                            </div>
+                        </div>)}
+                    </div>
+                </div>
+            </section>
             <Footer />
         </>
     )
