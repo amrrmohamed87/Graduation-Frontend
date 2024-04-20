ccc10483d89fca37bdbb5940f7c346fc54c371c4
Amr <frank1276smith@gmail.com>
2024-04-20 04:14:44 +0200

    adding new section for healthy food, and enhancing the ui of the page - amr

--- src/health/Nutrition.jsx
+++ src/health/Nutrition.jsx
@@ -1,6 +1,9 @@
 import heroImage from "../assets/images/nutrition-hero.jpg";
 import dietImage from "../assets/images/diet.jpg";
+import benefits from "../assets/images/diets.png";
 import CaloriesCalculator from "../components/CaloriesCalculator";
+import Footer from "@/components/Footer";
+import { healthyFood, healthyFoodTwo } from "@/data/constants";
 
 function Nutrition() {
   return (
@@ -18,26 +21,72 @@ function Nutrition() {
           </h1>
         </div>
       </section>
-      <section className="md:h-screen">
-        <h1 className="text-center text-emerald-700 text-[40px] mt-16 md:mb-12 md:mt-36 md:text-[60px]">
-          فوائد الغذاء الصحي
+      <section className="md:mb-36">
+        <h1 className="text-center text-emerald-700 text-[28px] mt-16 md:mb-10 md:mt-20 md:text-[45px]">
+          التغذية الصحية
         </h1>
-        <div className="mt-4 md:flex md:items-center">
-          <p className="text-right text-emerald-950 text-[25px] mx-4 mb-2 md:text-[35px]">
-            أعظم ثروة في الحياة هي الصحة. لذلك، من الضروري أن تعتني بصحتك. واحدة
-            من العناصر الأساسية لتصبح صحية هي التغذية السليمة. يعد تزويد جسمك
-            بالطعام الصحي أسلوبًا مهمًا يجب اتباعه. اجعل صحتك أولوية ولاحظ أنها
-            تغير كل جانب من جوانب حياتك
-          </p>
+        <div className="mt-0 md:mt-4 p-4 md:p-8 md:flex md:items-center gap-8">
+          <div className="bg-white border rounded-[10px] shadow-md mb-4 py-4">
+            <div className="flex justify-center items-center gap-2">
+              <h2 className="text-center mb-2 md:mb-6 pt-3 text-[17px] md:text-[25px]">
+                فوائد الغذاء الصحي
+              </h2>
+              <img src={benefits} className="w-[32px]" />
+            </div>
+            <p className="text-right text-emerald-950 text-[17px] mx-4 mb-2 md:text-[20px] pl-0 md:pl-6 tracking-normal pb-2 mb:pb-8">
+              أعظم ثروة في الحياة هي الصحة. لذلك، من الضروري أن تعتني بصحتك.
+              واحدة من العناصر الأساسية لتصبح صحية هي التغذية السليمة. يعد تزويد
+              جسمك بالطعام الصحي أسلوبًا مهمًا يجب اتباعه. اجعل صحتك أولوية
+              ولاحظ أنها تغير كل جانب من جوانب حياتك
+            </p>
+          </div>
           <img
             src={dietImage}
-            className="p-4 md:h-[450px] rounded-[50px] shadow-amber-50"
+            className="md:h-[380px] rounded-[10px] shadow-md"
           />
         </div>
       </section>
+
+      <section className="mb-16">
+        <div className="flex flex-col md:flex-row justify-center gap-12 p-4">
+          {healthyFood.map((food) => (
+            <div
+              key={food.src}
+              className="flex flex-col justify-center items-end max-w[300px] md:max-w-[400px] bg-white rounded-[15px] border shadow-md p-4"
+            >
+              <div className="flex items-center gap-2">
+                <img src={food.src} className="w-[30px]" />
+                <h2 className="mt-2 md:text-[22px]">{food.label}</h2>
+              </div>
+              <p className="text-right md:text-[16px] mt-2">
+                {food.description}
+              </p>
+            </div>
+          ))}
+        </div>
+
+        <div className="flex flex-col md:flex-row justify-center gap-12 p-4">
+          {healthyFoodTwo.map((food) => (
+            <div
+              key={food.src}
+              className="flex flex-col justify-center items-end max-w[300px] md:max-w-[400px] bg-white rounded-[15px] border shadow-md p-4"
+            >
+              <div className="flex items-center gap-2">
+                <img src={food.src} className="w-[30px]" />
+                <h2 className="mt-2 md:text-[22px]">{food.label}</h2>
+              </div>
+              <p className="text-right md:text-[16px] mt-2">
+                {food.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </section>
+
       <section>
         <CaloriesCalculator />
       </section>
+      <Footer />
     </main>
   );
 }
