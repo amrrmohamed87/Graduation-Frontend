5b644a8be1c6471dfe4e95891f78f0c1b3ef5289
Amr <frank1276smith@gmail.com>
2024-05-15 13:54:42 +0300

    Adding a new condition that if there's a token and the role is === patient, the show the new navigation menu && adding a new naviagtion menu. it's not done yet it needs some enhancement - amr

--- src/components/NewNavbar.jsx
+++ src/components/NewNavbar.jsx
@@ -14,12 +14,25 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
 import { TbLogout2 } from "react-icons/tb";
 
 import logo1 from "../assets/images/MHI.png";
 import logo2 from "../assets/images/MHI-Emerald.svg";
 
 function NewNavbar() {
+  const name = localStorage.getItem("patientName");
+  const role = localStorage.getItem("role");
   const [isScrolled, setIsScrolled] = useState(false);
   const [isSidebar, setIsSidebar] = useState(false);
   const token = useRouteLoaderData("root");
@@ -41,7 +54,9 @@ function NewNavbar() {
     };
   }, []);
 
-  function logoutHandler() {
+  function logoutHandler(event) {
+    event.preventDefault();
+    event.stopPropagation();
     submit(null, { action: "/logout", method: "post" });
   }
 
@@ -185,6 +200,73 @@ function NewNavbar() {
                 </AlertDialogContent>
               </AlertDialog>
             </li>
+          )}
+          {token && role === "patient" && (
+            <NavigationMenu className="mr-12">
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger className="text-[20px]">
+                    {name}
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="md:w-[400px] lg:w-[140px]">
+                      <li className="row-span-3">
+                        <NavigationMenuLink asChild>
+                          <div className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-2 no-underline outline-none focus:shadow-md">
+                            <NavLink
+                              to="/profile"
+                              className="mb-2 text-lg font-medium"
+                            >
+                              Profile
+                            </NavLink>
+                            <li
+                              className={`hidden md:block list-none text-[16px] hover:animate-pulse text-black`}
+                            >
+                              <AlertDialog>
+                                <AlertDialogTrigger asChild>
+                                  <button>تسجيل الخروج</button>
+                                </AlertDialogTrigger>
+                                <AlertDialogContent className="bg-white">
+                                  <AlertDialogHeader>
+                                    <AlertDialogTitle className="text-[#056558]">
+                                      هل أنت متأكد؟
+                                    </AlertDialogTitle>
+                                    <AlertDialogDescription className="text-emerald-700 text-[15px] md:text-[20px]">
+                                      سيؤدي هذا الإجراء إلى تسجيل خروجك نهائيًا
+                                      من حسابك ولن تعد متاحًا لاستخدام هذه
+                                      الخدمات حتى تقوم بتسجيل الدخول مرة أخرى
+                                    </AlertDialogDescription>
+                                  </AlertDialogHeader>
+                                  <AlertDialogFooter className="flex items-center gap-3">
+                                    <AlertDialogCancel className="border-2 border-[#056558] text-emerald-950 mb-1 text-[18px] font-bold">
+                                      إلغاء
+                                    </AlertDialogCancel>
+                                    <Form
+                                      action="/logout"
+                                      method="post"
+                                      onClick={logoutHandler}
+                                      className="flex items-center gap-2 bg-white shadow-2xl rounded-[30px] px-3 py-2 md:gap-8 cursor-pointer"
+                                    >
+                                      <TbLogout2
+                                        size={20}
+                                        className="text-emerald-950"
+                                      />
+                                      <button className="text-emerald-950">
+                                        تسجيل الخروج
+                                      </button>
+                                    </Form>
+                                  </AlertDialogFooter>
+                                </AlertDialogContent>
+                              </AlertDialog>
+                            </li>
+                          </div>
+                        </NavigationMenuLink>
+                      </li>
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
           )}
         </div>
 
