8e8c1f83a406ab561fa1bd316dc9efa9d8ccd4c2
rameeezz <ramezhossam92@gmail.com>
2024-06-22 16:51:35 +0300

    ..............

--- src/pages/DocSearch.jsx
+++ src/pages/DocSearch.jsx
@@ -794,7 +794,7 @@ export function DocSearch() {
                 >
                   <div className="position-absolute top-0 start-50 translate-middle StyleForDoctorCardIMg overflow-hidden">
                     <img
-                      src={`https://mhiproject.onrender.com/${element.image}`}
+                      src={`https://mhi-production.up.railway.app/${element.image}`}
                       alt=""
                       className="w-100 h-100"
                     />
